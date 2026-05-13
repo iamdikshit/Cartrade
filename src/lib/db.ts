bb1ff635008f99577ad5f33a8fa75892e6368f08
Dikshit Bhardwaj <dikshit.b.122@gmail.com>
2026-05-13 09:30:55 +0530

    db log fixed

--- src/lib/db.ts
+++ src/lib/db.ts
@@ -1,7 +1,7 @@
 import mongoose from "mongoose";
 
 const MONGODB_URI = process.env.MONGODB_URI!;
-console.log("🔗 MongoDB URI:", MONGODB_URI); // Log the MongoDB URI for debugging
+
 if (!MONGODB_URI) {
   throw new Error("Please define the MONGODB_URI environment variable");
 }
@@ -33,7 +33,7 @@ async function connectDB(): Promise<typeof mongoose> {
     };
 
     cached.promise = mongoose.connect(MONGODB_URI, opts).then((m) => {
-      console.log("✅ MongoDB connected " + MONGODB_URI);
+      console.log("✅ MongoDB connected ");
       return m;
     });
   }
